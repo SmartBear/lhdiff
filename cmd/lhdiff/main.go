93286820c56a2dcd37b66c99e88768f5f8c20941
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-11-03 13:26:26 +0000

    Add --omit flag

--- cmd/lhdiff/main.go
+++ cmd/lhdiff/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
+	omit := flag.Bool("omit", false, "Omit lines with same line numbers from output")
 	flag.Parse()
 	leftFile := flag.Arg(0)
 	rightFile := flag.Arg(1)
 	left, _ := ioutil.ReadFile(leftFile)
 	right, _ := ioutil.ReadFile(rightFile)
 	linePairs, leftCount, newRightLines := lhdiff.Lhdiff(string(left), string(right), 4)
-	lhdiff.PrintLinePairs(linePairs, leftCount, newRightLines, false)
+	lhdiff.PrintLinePairs(linePairs, leftCount, newRightLines, *omit)
 }
